8fa6142cd165a1e9f381011d7df35036d4d5808e
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-02 17:03:49 +0800

    Adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <Navbar />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <Navbar />
 
-      <div className="container-fluid">
-        <div className="container-md py-5">
-          <h2 className="text-center text-white container-fluid animate__animated animate__zoomIn">
-            Twitter Video Downloader
-          </h2>
-          <br />
-          <Form />
-        </div>
-      </div>
-    </div>
+            <div className="container-fluid">
+              <div className="container-md py-5">
+                <h2 className="text-center text-white container-fluid animate__animated animate__zoomIn">
+                  Twitter Video Downloader
+                </h2>
+                <br />
+                <Form />
+              </div>
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
