09b30c093cd73ed106cb83d56d7b4cd8f4b0bc22
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-04 02:00:35 +0800

    Remove React Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
