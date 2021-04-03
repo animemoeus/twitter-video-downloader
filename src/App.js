f6f59ee1f1affd4a5749a9b3dcbd8e6e3edc7251
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-03 22:09:48 +0800

    Adding Loader Before Component Mount

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
+import { Spinner } from "./components/loader";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>
-            <Navbar />
+  const [isLoading, setIsLoading] = useState(true);
 
-            <div className="container-fluid">
-              <div className="container-md py-5">
-                <h2 className="text-center text-white container-fluid animate__animated animate__zoomIn">
-                  Twitter Video Downloader
-                </h2>
-                <br />
-                <Form />
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <Navbar />
+
+              <div className="container-fluid">
+                <div className="container-md py-5">
+                  <h2 className="text-center text-white container-fluid animate__animated animate__zoomIn">
+                    Twitter Video Downloader
+                  </h2>
+                  <br />
+                  <Form />
+                </div>
               </div>
             </div>
-          </div>
-        </Route>
-      </Switch>
-    </Router>
-  );
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/loader/index.js
@@ -0,0 +1,25 @@
+function Spinner() {
+  return (
+    <div
+      className="lds-grid"
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export { Spinner };
