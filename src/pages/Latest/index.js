75157006e67bebe8d4db0e1d90f0ef9f8456c5b1
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-20 21:13:43 +0800

    adding ads

--- src/pages/Latest/index.js
+++ src/pages/Latest/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
+import { Ads } from "../../components/atoms";
 import { Navbar } from "../../components/molecules";
 
 function Latest() {
@@ -45,39 +46,50 @@ function Latest() {
             <h1 className="text-white text-center">Latest Downloaded Video</h1>
 
             <ResponsiveMasonry
-              columnsCountBreakPoints={{ 350: 2, 750: 2, 900: 3, 1100: 4 }}
+              columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 3, 1100: 4 }}
             >
               <Masonry>
                 {tweets.map((tweet, index) => {
-                  return (
-                    <div
-                      className="p-1 align-items-start animate__animated animate__fadeIn"
-                      key={index}
-                    >
+                  if (index % 7 === 0) {
+                    return (
                       <div
-                        className="card grid-item"
-                        style={{ width: "100%", borderRadius: 0 }}
+                        className="p-1 align-items-start animate__animated animate__fadeIn"
+                        key={index}
                       >
-                        <img
-                          src={tweet.thumbnail}
-                          className="card-img-top shadow-sm"
-                          alt={tweet.name}
-                        />
-                        <hr className="m-0" />
-                        <div className="card-body text-center p-0 pb-2">
-                          <p className="card-title fs-5">{tweet.name}</p>
-                          <a
-                            href={`http://ouo.io/qs/Ks3CJSWC?s=${tweet.video}`}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="btn btn-primary btn-sm shadow-sm"
-                          >
-                            Download
-                          </a>
+                        <Ads />
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        className="p-1 align-items-start animate__animated animate__fadeIn"
+                        key={index}
+                      >
+                        <div
+                          className="card grid-item"
+                          style={{ width: "100%", borderRadius: 0 }}
+                        >
+                          <img
+                            src={tweet.thumbnail}
+                            className="card-img-top shadow-sm"
+                            alt={tweet.name}
+                          />
+                          <hr className="m-0" />
+                          <div className="card-body text-center p-0 pb-2">
+                            <p className="card-title fs-5">{tweet.name}</p>
+                            <a
+                              href={`http://ouo.io/qs/Ks3CJSWC?s=${tweet.video}`}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="btn btn-primary btn-sm shadow-sm"
+                            >
+                              Download
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
+                    );
+                  }
                 })}
               </Masonry>
             </ResponsiveMasonry>
