3ad01900174f6afc2ffe33ae211179ab39d32e51
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-20 20:52:45 +0800

    adding Spinner

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light animate__animated animate__fadeInDown shadow-sm">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light animate__animated animate__fadeInDown shadow">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           Twitter Video Downloader
