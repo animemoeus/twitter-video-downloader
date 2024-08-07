0b6d4dbedd3d6b11a6f6339b28ba29f58f57fc1a
Arter Tendean <arter.tendean.07@gmail.com>
2024-08-08 01:07:33 +0700

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useTelegramWebApp } from "@/hooks/useTelegram";
 
 export default function Home() {
   const telegramWebApp = useTelegramWebApp();
   if (!telegramWebApp) {
-    return <p>✪ ω ✪</p>;
+    return (
+      <div className="flex min-h-screen justify-center items-center">
+        <h3>✪ ω ✪</h3>
+      </div>
+    );
   }
-  const handleClick = () => {
-    console.log("hello");
-  };
-
-  console.log("arter");
-  console.log(telegramWebApp);
 
   return (
     <div className="flex min-h-screen justify-center items-center">
       <div>Hello World!</div>
-      <button onClick={handleClick}>Test`</button>
     </div>
   );
 }
